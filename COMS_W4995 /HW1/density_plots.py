6ac6c704453df0ccdc168f8122daa4f83126b193
notjdr <jacektomek21@gmail.com>
2024-12-25 10:07:22 +0000

    Initial Commit

---
+++ COMS_W4995 /HW1/density_plots.py
@@ -0,0 +1,20 @@
+# %% [markdown]
+"""
+# Task 1: Density Plots (50 points)
+
+1.1. Plot the longtitude vs latitude several ways within a single figure (each in its own axes)
+1. Using the matplotlib defaults.
+2. Adjusting alpha and marker size to compensate for overplotting.
+3. Using a hexbin plot.
+4. Subsampling the dataset. 
+<p>
+For each but the first one, ensure that all the plotting area is used in a reasonable way and that as much information as possible is conveyed; 
+this is somewhat subjective and there is no one right answer. <b>[45pts]</b>
+<p>
+1.2 In what areas are most of the anomalies (measurements) located? <b>[5pts]</b>
+"""
+
+# %% Inport modules
+
+
+
