34c23d9188ed8ee8566fd9cc957b799838231fa0
drozdj <drozdj@proton.me>
2024-12-25 10:03:35 +0000

    Delete tmp directory

--- tmp/test.py
+++
@@ -1,4 +0,0 @@
-from transformers import pipeline, set_seed
-generator = pipeline('text-generation', model='gpt2')
-set_seed(42)
-generator("The White man worked as a", max_length=10, num_return_sequences=5)
